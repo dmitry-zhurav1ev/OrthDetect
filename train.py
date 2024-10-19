99f2f77e86f422167ed5b798d35082190bdf3878
dmitry-zhurav1ev <102475542+dmitry-zhurav1ev@users.noreply.github.com>
2024-10-20 00:09:13 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,43 @@
-﻿import os
+import os
 from ultralytics import YOLO
+import tkinter as tk
+from PIL import Image, ImageTk
+from datetime import datetime
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
+# Вывод метрик на экран после обучения
+def find_latest_directory(directory):
+    items = os.listdir(directory)
+    directories = [os.path.join(directory, item) for item in items if os.path.isdir(os.path.join(directory, item))]
+    if not directories:
+        return None
+    latest_directory = max(directories, key=os.path.getctime)
+    
+    return latest_directory
+
+def display_image(image_path):
+    window = tk.Tk()
+    window.title("Отображение изображения")
+
+    img = Image.open(image_path)
+    
+    window_width = 800
+    window_height = 400
+    window.geometry(f"{window_width}x{window_height}")
+
+    img = img.resize((window_width, window_height), Image.LANCZOS)
+    
+    img_tk = ImageTk.PhotoImage(img)
+
+    label = tk.Label(window, image=img_tk)
+    label.pack(expand=True)
+
+    window.mainloop()
 
 model = YOLO("yolo11m.pt")  # load a pretrained model (recommended for training)
 
 if __name__ ==  '__main__':
-    results = model.train(data=os.path.join(script_dir, 'dataset_teeth', 'data.yaml'), epochs=50, imgsz=640, workers = 2, lr0=0.001)
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    results = model.train(data=os.path.join(script_dir, 'dataset_teeth6', 'data.yaml'), epochs=3, imgsz=640, workers = 2, lr0=0.001)
+    runs_dir = os.path.join(script_dir, "runs", "detect")
+    image_path = os.path.join(find_latest_directory(runs_dir), "results.png")
+    display_image(image_path)
